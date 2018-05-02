5f4f93f6d2eaec9cfacca6043a5c90646c1fd708
Ne4istb <anechytailov@gmail.com>
2018-05-02 20:43:05 +0300

    Fixed smart contract deployer

--- SmartContracts/SmartContractDeployer/Program.cs
+++ SmartContracts/SmartContractDeployer/Program.cs
@@ -3,6 +3,7 @@ using Nethereum.Web3;
 using Nethereum.RPC.Eth;
 using System.Threading.Tasks;
 using System.IO;
+using Nethereum.Hex.HexTypes;
 
 namespace SmartContractDeployer
 {
@@ -17,29 +18,35 @@ namespace SmartContractDeployer
 
         public static async Task MainAsync(string[] args)
         {
-            var server = args[0];
-            var binFileName = args[1];
+            //var server = args[0];
+            //var binFileName = args[1];
+            var server = "http://localhost:8545/";
+            var binFileName = "/Users/Ne4istb/Sources/Xamarin/RipOffMotivator/SmartContracts/storage_sol_Storage.bin";
 
-            var addres = await DeployAsync(server, binFileName);
-
-            Console.Write(addres);
+            try
+            {
+                var address = await DeployAsync(server, binFileName);
+                Console.Write(address);
+            }
+            catch (Exception ex){
+                Console.WriteLine($"Something went wrong: {ex.Message}");
+            }
         }
 
         async static Task<string> DeployAsync(string server, string binFileName)
         {
             var web3 = new Web3(server);
 
-            //var createAccount = web3.Personal.NewAccount;
-            //var newAccount = await createAccount.SendRequestAsync(DeployAccountPass);
-
             var coinbase = new EthCoinBase(web3.Client);
             await web3.Personal.UnlockAccount.SendRequestAsync(coinbase, DeployAccountPass);
 
             var bin = ReadFileContent(binFileName);
+            var fromAddress = await web3.Eth.CoinBase.SendRequestAsync();
+            var gas = new HexBigInteger(1000000);
 
-            var receipt = await web3.Eth.DeployContract.SendRequestAndWaitForReceiptAsync(bin, coinbase.ToString());
+            var receipt = await web3.Eth.DeployContract.SendRequestAndWaitForReceiptAsync(bin, fromAddress, gas);
 
-            return receipt.ToString();
+            return receipt.ContractAddress;
         }
 
         static string ReadFileContent(string fileName){
