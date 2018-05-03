7f5860cf3bd0093afc9e8182fef5fa28b362801d
Ne4istb <anechytailov@gmail.com>
2018-05-03 11:44:46 +0300

    Added smart contract interface

--- RipOffMotivator/SmartContractsProxy/SmartContractsProxy.cs
+++ RipOffMotivator/SmartContractsProxy/SmartContractsProxy.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Nethereum.Contracts;
+using Nethereum.Hex.HexTypes;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Web3;
 
@@ -8,32 +10,69 @@ namespace SmartContractsProxy
 {
     public class SmartContractsProxy : ISmartContractsProxy
     {
-        const string Abi = "";
-        const string Adress = "";
-        const string senderAddress = "";
+        // TODO: move to config
+        const string AbiFileName = "/Users/Ne4istb/Sources/Xamarin/RipOffMotivator/SmartContracts/storage_sol_Storage.abi";
+        const string ContractAdress = "0x8ff4f1044b46e5b2c4e91a89cdff272a59573caa";
+        const string SenderAddress = "";
+
         readonly Web3 web3;
         readonly Contract contract;
 
         public SmartContractsProxy(){
             web3 = new Web3();
-            contract = web3.Eth.GetContract(Abi, Adress);
+            var abi = ReadFileContent(AbiFileName);
+
+            contract = web3.Eth.GetContract(abi, ContractAdress);
         }
 
-        public void Test (){
-                
-        }
-
-        public void AddGoal(decimal amount, DateTime executionTime, Guid rejectTriggerId)
+        public async Task<string> AddGoalAsync(decimal amount, DateTime executionTime, Guid rejectTriggerId)
         {
-            var addGoal = contract.GetFunction("addGoal");
+            var set = contract.GetFunction("set");
+
+            var fromAddress = await GetFromAddress();
+            var gas = new HexBigInteger(1000000);
+
+            var receipt = await set.SendTransactionAsync(fromAddress, gas, gas, null, 42);
+            return receipt;
+
+            //var addGoal = contract.GetFunction("addGoal");
         }
 
-        public async void RejectAsync(Guid triggerId)
+        public async Task<string> RejectAsync(Guid triggerId)
         {
-            var rejectFunc = contract.GetFunction("reject");
+            var get = contract.GetFunction("get");
 
-            var receipt = await rejectFunc
-                .SendTransactionAndWaitForReceiptAsync(senderAddress, null, triggerId.ToString());
+            var fromAddress = await GetFromAddress();
+            var gas = new HexBigInteger(1000000);
+
+            var input = new TransactionInput(null, gas, fromAddress);
+            var output = await get.CallAsync<int>();
+
+            return output.ToString();
+
+            //var rejectFunc = contract.GetFunction("reject");
+
+            //var receipt = await rejectFunc
+                //.SendTransactionAndWaitForReceiptAsync(SenderAddress, null, triggerId.ToString());
+        }
+
+        Task<string> GetFromAddress(){
+            return web3.Eth.CoinBase.SendRequestAsync();
+        }
+
+        static string ReadFileContent(string fileName)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Cannot read {fileName}", e);
+            }
         }
     }
 }
---
+++ SmartContracts/Contracts/RipOffMotivator.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.21;
+contract RipOffMotivator {
+
+  function addGoal(int amount, uint256 executionTime, string rejectTriggerId) constant returns (string) {
+    return rejectTriggerId;
+  }
+
+  function reject(string rejectTriggerId) constant returns (string) {
+    return rejectTriggerId;
+  }
+}
