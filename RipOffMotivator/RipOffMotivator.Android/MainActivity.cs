7d3b6c66ee76380f49f9bc66ec530e235c32d3db
unknown <zelikmananna2015@gmail.com>
2018-05-03 15:34:54 +0300

    Fixed tag reading

--- RipOffMotivator/RipOffMotivator.Android/MainActivity.cs
+++ RipOffMotivator/RipOffMotivator.Android/MainActivity.cs
@@ -3,17 +3,26 @@ using System.Linq;
 
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
+using Android.Nfc;
 using Android.OS;
 
 
 namespace RipOffMotivator.Droid
 {
-    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MainTheme")]
-	[IntentFilter(new[] { Intent.ActionMain })]
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MainTheme", LaunchMode = LaunchMode.SingleTop)]
+	[IntentFilter(new[] { Intent.ActionMain, "android.nfc.action.TAG_DISCOVERED" })]
+    [IntentFilter(new[] { "android.nfc.action.NDEF_DISCOVERED" },
+        DataMimeType = ViewApeMimeType,
+        Categories = new[] { "android.intent.category.DEFAULT" })]
+
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
-		internal static MainActivity Instance { get; private set; }
+        const string ViewApeMimeType = "application/vnd.xamarin.nfcxample";
 
+        internal static MainActivity Instance { get; private set; }
+
+        static NFCIntegration nfc;
 		protected override void OnCreate(Bundle bundle)
 		{
 			TabLayoutResource = Resource.Layout.Tabbar;
@@ -25,13 +34,64 @@ namespace RipOffMotivator.Droid
 			Xamarin.Forms.Forms.Init(this, bundle);
 
 			Xamarin.Forms.DependencyService.Register<INFCIntegration, NFCIntegration>();
-			((NFCIntegration)Xamarin.Forms.DependencyService.Get<INFCIntegration>()).SetContext(this);
 
-            //var nfc = new NFCIntegration();
-            //nfc.SetContext(this);
+            nfc = ((NFCIntegration)Xamarin.Forms.DependencyService.Get<INFCIntegration>());
 
-			LoadApplication(new App());
+            LoadApplication(new App());
 		}
-	}
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            if (nfc.NfcAdapter == null)
+            { 
+                var alert = new AlertDialog.Builder(this).Create();
+                alert.SetMessage("NFC is not supported on this device.");
+                alert.SetTitle("NFC Unavailable");
+                alert.Show();
+            }
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+
+            var intentAction = intent.Action ?? String.Empty;
+            var intentType = intent.Type ?? String.Empty;
+            if (nfc.InWriteMode)
+            {
+                nfc.AdjustNFCTag(intent);
+                return;
+            }
+
+            if (!intentAction.Equals("android.nfc.action.TAG_DISCOVERED") && !intentType.Equals(ViewApeMimeType))
+                return;
+
+            var rawMessages = intent.GetParcelableArrayExtra(NfcAdapter.ExtraNdefMessages);
+            if (rawMessages == null)
+                return;
+
+            var record = (((NdefMessage)rawMessages[0]).GetRecords())[0];
+
+            intent.PutExtra("Id", record.GetId());
+            StartService(intent);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            var tagDetected = new IntentFilter(NfcAdapter.ActionTagDiscovered);
+            var filters = new[] { tagDetected };
+
+            var intent = new Intent(this, GetType()).AddFlags(ActivityFlags.SingleTop);
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+            nfc.NfcAdapter.EnableForegroundDispatch(this, pendingIntent, filters, null);
+        }
+
+        public static void EnableWriteMode() {
+            nfc.InWriteMode = true;
+        }
+    }
 }
 
--- RipOffMotivator/RipOffMotivator.Android/NFCIntegration.cs
+++ RipOffMotivator/RipOffMotivator.Android/NFCIntegration.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-
+using System.IO;
+using System.Text;
 using Android.App;
 using Android.Content;
 using Android.Nfc;
 using Android.Nfc.Tech;
+using Android.OS;
 
 namespace RipOffMotivator.Droid
 {
@@ -11,9 +13,9 @@ namespace RipOffMotivator.Droid
     {
         const string ViewApeMimeType = "application/vnd.xamarin.nfcxample";
 
-        NfcAdapter NfcAdapter;
+        public NfcAdapter NfcAdapter;
         Context Context;
-        Boolean InWriteMode = false;
+        public Boolean InWriteMode = false;
         string Message = string.Empty;
         Action<Guid, string> Action;
 
@@ -26,67 +28,14 @@ namespace RipOffMotivator.Droid
 		public void SetContext(Context ctx)
 		{
 			Context = ctx;
-		}
-
-		void EnableWriteMode()
+        }
+        
+        void EnableWriteMode()
         {
-            InWriteMode = true;
-
-            var tagDetected = new IntentFilter(NfcAdapter.ActionTagDiscovered);
-            var filters = new[] { tagDetected };
-
-            var intent = new Intent(Context, GetType()).AddFlags(ActivityFlags.SingleTop);
-            var pendingIntent = PendingIntent.GetActivity(Context, 0, intent, 0);
-           
-            if (NfcAdapter != null)
-            {
-                NfcAdapter.EnableForegroundDispatch(getActivity(Context), pendingIntent, filters, null);
-            }
-            else
-            {
-                var alert = new AlertDialog.Builder(Context).Create();
-                alert.SetMessage("NFC is not supported on this device.");
-                alert.SetTitle("NFC Unavailable");
-                alert.Show();
-            }
+            MainActivity.EnableWriteMode();
         }
 
-        public Activity getActivity(Context context)
-        {
-            if (context == null)
-                return null;
-            if (context is ContextWrapper)
-            {
-                if (context is Activity)
-                    return (Activity)context;
-
-                return getActivity(((ContextWrapper)context).BaseContext);
-            }
-
-            return null;
-        }
-
-        protected override void OnNewIntent(Intent intent)
-        {
-            var intentType = intent.Type ?? String.Empty;
-
-            if (InWriteMode)
-            {
-                AdjustNFCTag(intent);
-                return;
-            }
-
-            if (!ViewApeMimeType.Equals(intentType, StringComparison.OrdinalIgnoreCase))
-                return;
-
-            var rawMessages = intent.GetParcelableArrayExtra(NfcAdapter.ExtraNdefMessages);
-            var record = (((NdefMessage)rawMessages[0]).GetRecords())[0];
-
-            intent.PutExtra("Id", record.GetId());
-            StartService(intent);
-        }
-
-        void AdjustNFCTag(Intent intent)
+        public void AdjustNFCTag(Intent intent)
         {
             InWriteMode = false;
             var tag = intent.GetParcelableExtra(NfcAdapter.ExtraTag) as Tag;
@@ -110,20 +59,47 @@ namespace RipOffMotivator.Droid
             if (ndef == null || !ndef.IsWritable)
                 return false;
 
-            var mimeBytes = Convert.FromBase64String(ViewApeMimeType);
+            var mimeBytes = Encoding.ASCII.GetBytes(ViewApeMimeType);
             var id = tag.GetId();
-            var apeRecord = new NdefRecord(NdefRecord.TnfMimeMedia, mimeBytes, id, Convert.FromBase64String(Message));
+            var apeRecord = new NdefRecord(NdefRecord.TnfMimeMedia, mimeBytes, id, Encoding.ASCII.GetBytes(Message));
             var ndefMessage = new NdefMessage(new[] { apeRecord });
-
-            ndef.Connect();
 
             if (ndef.MaxSize < ndefMessage.ToByteArray().Length)
                 return false;
 
-            ndef.WriteNdefMessage(ndefMessage);
+            try
+            {
+                ndef.Connect();
+                ndef.WriteNdefMessage(ndefMessage);
 
-            Action(new Guid(id), Message);
-            return true;
+                Action(new Guid(id), Message);
+                ndef.Close();
+                return true;
+            }
+            catch (Java.IO.IOException)
+            {
+                var format = NdefFormatable.Get(tag);
+                if (format == null)
+                {
+                    return false;
+                    //DisplayMessage("Tag does not appear to support NDEF format.");
+                }
+                else
+                {
+                    try
+                    {
+                        format.Connect();
+                        format.Format(ndefMessage);
+                        Action(new Guid(id), Message);
+                        format.Close();
+                        return true;
+                    }
+                    catch (IOException)
+                    {
+                        return false;
+                    }
+                }
+            }
         }
 
         public void CreateNFCTag(string message, Action<Guid, string> action)
